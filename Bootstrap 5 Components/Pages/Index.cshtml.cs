02c8c964267d092bbd313320d2d3e45017bbb5bf
Dean Leigh <dean.leigh@wholething.co.uk>
2022-11-12 17:01:16 +0000

    Added test code in Razor

--- Bootstrap 5 Components/Pages/Index.cshtml.cs
+++ Bootstrap 5 Components/Pages/Index.cshtml.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bootstrap_5_Components.Models;
+using Bootstrap_5_Components.Services;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Bootstrap_5_Components.Pages
@@ -7,14 +9,16 @@ namespace Bootstrap_5_Components.Pages
     {
         private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger,
+    JsonFileProductService productService)
         {
             _logger = logger;
+            ProductService = productService;
         }
 
-        public void OnGet()
-        {
+        public JsonFileProductService ProductService { get; }
+        public IEnumerable<Product> Products { get; private set; }
 
-        }
+        public void OnGet() => Products = ProductService.GetProducts();
     }
 }
