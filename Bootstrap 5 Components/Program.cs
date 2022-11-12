e096d1b00c968975359d9c0ac09aafc58e269f72
Dean Leigh <dean.leigh@wholething.co.uk>
2022-11-12 14:43:52 +0000

    Added Program and StartUp code from https://github.com/bradygaster/ContosoCrafts

--- Bootstrap 5 Components/Program.cs
+++ Bootstrap 5 Components/Program.cs
@@ -1,3 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -23,3 +33,21 @@ app.UseAuthorization();
 app.MapRazorPages();
 
 app.Run();
+
+namespace Bootstrap_5_Components
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
