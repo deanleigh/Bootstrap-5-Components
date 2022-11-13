6677763075ae7954b498b6daa0ddf0e39886cba2
Dean Leigh <dean.leigh@wholething.co.uk>
2022-11-13 10:09:12 +0000

    Connected data to razor template

--- Bootstrap 5 Components/Pages/Cards.cshtml.cs
+++ Bootstrap 5 Components/Pages/Cards.cshtml.cs
@@ -1,3 +1,5 @@
+using Bootstrap_5_Components.Models;
+using Bootstrap_5_Components.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,18 @@ namespace Bootstrap_5_Components.Pages
 {
     public class CardsModel : PageModel
     {
-        public void OnGet()
+        private readonly ILogger<CardsModel> _logger;
+
+        public CardsModel(ILogger<CardsModel> logger,
+    JsonFileProductService productService)
         {
+            _logger = logger;
+            ProductService = productService;
         }
+
+        public JsonFileProductService ProductService { get; }
+        public IEnumerable<Product> Products { get; private set; }
+
+        public void OnGet() => Products = ProductService.GetProducts();
     }
 }
