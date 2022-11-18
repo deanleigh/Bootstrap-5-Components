4caf65307938ee0d5f57d12727082bea01f092e2
Dean Leigh <dean.leigh@wholething.co.uk>
2022-11-18 08:25:59 +0000

    Added iterator to slides Still need to add to controls

--- Bootstrap 5 Components/Pages/Slides.cshtml.cs
+++ Bootstrap 5 Components/Pages/Slides.cshtml.cs
@@ -1,3 +1,5 @@
+using Bootstrap_5_Components.Models;
+using Bootstrap_5_Components.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,24 @@ namespace Bootstrap_5_Components.Pages
 {
     public class SlidesModel : PageModel
     {
-        public void OnGet()
+        private readonly ILogger<SlidesModel> _logger;
+        public SlidesModel(ILogger<SlidesModel> logger,
+    JsonFileProductService productService)
         {
+            _logger = logger;
+            ProductService = productService;
         }
+
+        public JsonFileProductService ProductService { get; }
+        public IEnumerable<Product> Products { get; private set; }
+
+        public void OnGet() => Products = ProductService.GetProducts();
     }
 }
+
+
+
+
+
+
+
