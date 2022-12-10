1c30a80f76b084392d24d315cb2801eb850fe3dc
Dean Leigh <dean.leigh@wholething.co.uk>
2022-12-10 14:48:08 +0000

    Added Bootstrap CSS Grid and created example page

---
+++ Bootstrap 5 Components/Pages/Grids.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Bootstrap_5_Components.Pages
+{
+    public class GridsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
